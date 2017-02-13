5d2c9b8ad5b309e7904c5d1def933f72a9ef9276
tolves <tov.sharing@gmail.com>
2017-02-13 18:04:22 +0800

    inline mode keyboard added

--- start.rb
+++ start.rb
@@ -11,27 +11,33 @@ require_relative  'db_connect'
 conn = Db.new
 client = conn.connect
 
+keyboard = [
+		Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Button 1', callback_data: 'first_btn'),
+		Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Button 2', callback_data: 'second_btn')
+]
+markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard)
+
+
 
 Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
 	bot.logger.info('Bot has been started')
 	bot.listen do |message|
-
+		bot.api.send_message chat_id: message.chat.id, text: 'Foo', reply_markup: markup
 		# begin
 			#next if message.date < (Time.now - 120).to_i
 			#case message.text
 			#	when /\/q ./
 				# bot.api.send_message(bot.api.methods)
 				bot.logger.info(bot.api.get_me)
-				# bot.api.send_message(chat_id: message.chat.id, text: message.date)
-				# bot.api.send_message(chat_id: message.chat.id, text: "出错辣,豆腐丝快粗来化身水管道工人")
-				# bot.api.sendPhoto(chat_id:message.chat.id,photo:bot.api.getUserProfilePhotos(75708608))
-				# bot.api.send_photo(chat_id:message.chat.id,photo:(bot.api.get_user_profile_photos(user_id:message.from.id))[0].file_id)
-				# bot.logger.info((bot.api.get_user_profile_photos(user_id:message.from.id,limit:1))['result']['photos'][0][0]['file_id'])
-				bot.api.get_user_profile_photos(user_id:message.from.id,limit:3)['result']['photos'].each do |photo|
+				# bot.api.inline_query(id:message.chat.id,from:message.from,query:'123',offset:'123123')
+
+
+
+				# bot.api.get_user_profile_photos(user_id:message.from.id,limit:3)['result']['photos'].each do |photo|
 					# bot.logger.info(photo)
-					bot.api.send_photo(chat_id: message.chat.id, photo:photo[0]['file_id'])
+					# bot.api.send_photo(chat_id: message.chat.id, photo:photo[0]['file_id'])
 					# bot.logger.info(photo[0]['file_id'])
-				end
+				# end
 					# bot.api.send_photo(chat_id: message.chat.id, photo:items.file_id)
 			# }
 			#end
