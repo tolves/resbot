903ca9f5dd5c072743d609be640a54d0be66c750
tolves <tov.sharing@gmail.com>
2017-02-14 11:07:24 +0800

    inline mode + answer callback

--- start.rb
+++ start.rb
@@ -10,7 +10,6 @@ token = '344686538:AAGWv2ANcHGZhoZDvubC5xMF2l9bOdJnh9k'
 require_relative  'db_connect'
 conn = Db.new
 client = conn.connect
-
 keyboard = [
 		Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Button 1', callback_data: 'first_btn'),
 		Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Button 2', callback_data: 'second_btn')
@@ -22,15 +21,24 @@ markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboar
 Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
 	bot.logger.info('Bot has been started')
 	bot.listen do |message|
-		bot.api.send_message chat_id: message.chat.id, text: 'Foo', reply_markup: markup
+		if(!message.respond_to? 'inline_message_id')
+			bot.api.send_message chat_id: message.chat.id, text: 'Foo', reply_markup: markup
+			bot.logger.info(message.chat.id)
+		else
+			bot.logger.info(message.id)
+			bot.api.answer_callback_query callback_query_id:message.id , text: 'inline'
+		end
+
+		# bot.api.send_message chat_id: message.chat.id, text: message.type
+		# bot.logger.info(message.callbackquery.inline_message_id)
 		# begin
 			#next if message.date < (Time.now - 120).to_i
 			#case message.text
 			#	when /\/q ./
 				# bot.api.send_message(bot.api.methods)
-				bot.logger.info(bot.api.get_me)
+				# bot.logger.info(bot.api.get_me)
 				# bot.api.inline_query(id:message.chat.id,from:message.from,query:'123',offset:'123123')
-
+				# bot.api.send_message chat_id:message.chat.id, text: 'Foo', reply_markup: markup
 
 
 				# bot.api.get_user_profile_photos(user_id:message.from.id,limit:3)['result']['photos'].each do |photo|
